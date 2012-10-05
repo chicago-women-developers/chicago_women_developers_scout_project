a31e50c9d676efd04a0dcb01153e3e44bc95230f
Laura <laura@Laura-linux.(none)>
2012-10-04 20:21:21 -0500

    stuff

--- scouts/app/models.py
+++ scouts/app/models.py
@@ -7,6 +7,12 @@ from google.appengine.api import users
 
 import mc
 
+import webapp2
+import cgi
+from protorpc import messages
+from google.appengine.ext import db
+from google.appengine.api import users
+import os
 
 class UserPrefs(db.Model):
     """Storage for custom properties related to a user. Provides caching
@@ -146,3 +152,90 @@ class YourCustomModel(db.Model):
     demo_boolean_property = db.BooleanProperty(default=True)
     demo_integer_property = db.IntegerProperty(default=1)
     demo_datetime_property = db.DateTimeProperty(auto_now_add=True)
+
+class User(db.Model):
+    user=db.UserProperty(required=True)
+    createdate = db.DateTimeProperty(auto_now_add=True)
+    role=db.IntegerProperty(required=True)
+
+
+class UserManager:
+    def checkUserExists(self, user): #I have no idea why we need to pass self, but it fixes this error: checkUserExists() takes exactly 1 argument (2 given)
+        userToCheck = db.GqlQuery("SELECT * FROM User WHERE user = :1 ",user)
+        if not userToCheck.get():
+            return False
+        else:
+            return True
+
+    def insertUser(self, user, role):
+        userToInsert = User(user=user, role=role)
+        userToInsert.put()
+
+class Roles(messages.Enum):
+    ADMIN = 1
+    PARENT = 2
+    SCOUT = 3
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        siteUser = UserManager()
+        if user:
+            userExists = siteUser.checkUserExists(user)
+            if not userExists:
+                self.redirect("/scoutregistration")
+            else:
+                self.response.out.write('Welcome ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+
+class SiteRegistrationHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('register for the site (get)')
+
+    def post(self):
+        self.response.write('registerfor the site (post)')
+
+
+class ScoutRegistrationHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            template_values = {"username":user.nickname()}
+            template = jinja_environment.get_template('registrationform.html')
+            self.response.out.write(template.render(template_values))
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+        
+    def post(self):
+        user = users.get_current_user()
+        isParent = self.request.get('isParent')
+        siteUser = UserManager()
+        if isParent:
+            siteUser.insertUser(user, int(Roles.PARENT))
+        else:
+            siteUser.insertUser(user, int(Roles.SCOUT))
+        self.response.write(welcomeForm % {"username":user.nickname()})
+
+
+class CreateEventHandler(webapp2.RequestHandler):
+    def get(self):
+        name = self.request.get_all()
+        self.response.write('Event is called: ' + name[0])
+        
+    def post(self):
+        self.response.write('create event post')
+        
+
+class ViewEventHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('view event (get)')
+
+    def post(self):
+        self.response.write('view event (post)')
